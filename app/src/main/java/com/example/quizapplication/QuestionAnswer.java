215d6108cf1169f14dde20cb13c38d61444432de
dhruvin2511 <dhruvinkotadiya25@gmail.com>
2024-06-27 15:38:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,43 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "What is the capital of France?",
+            "What is 5 + 7?",
+            "Who wrote 'Romeo and Juliet'?",
+            "What is the largest planet in our solar system?",
+            "How many continents are there on Earth?",
+            "What is the chemical symbol for water?",
+            "Who was the first President of the United States?",
+            "What is the smallest prime number?",
+            "What is the process by which plants make their food?",
+            "What is the main ingredient in guacamole?"
+    };
+
+    public static String choices [][]={
+            {"London" , "Berlin" , "Paris" , "Madrid"},
+            {"10", "11", "12", "13"},
+            {"Charles Dickens", "William Shakespeare", "Mark Twain", "J.K. Rowling"},
+            {"Earth", "Mars", "Jupiter", "Saturn"},
+            {"Five", "Six", "Seven", "Eight"},
+            {"CO2", "H2O", "O2", "NaCl"},
+            {"Thomas Jefferson", "Abraham Lincoln", "John Adams", "George Washington"},
+            {"1", "2", "3", "5"},
+            {"Respiration", "Digestion", "Photosynthesis", "Fermentation"},
+            {"Tomato", "Onion", "Avocado", "Pepper"}
+    };
+
+    public static String correctAnswers []={
+            "Paris",
+            "12",
+            "William Shakespeare",
+            "Jupiter",
+            "Seven",
+            "H2O",
+            "George Washington",
+            "2",
+            "Photosynthesis",
+            "Avocado"
+    };
+}
